4d5ef74f295f3990ad1b8b62d27c5d9726b7bcb8
Unkn0wn126 <32388228+Unkn0wn126@users.noreply.github.com>
2022-04-15 09:51:14 +0200

    Began changing the structure

--- src/App.js
+++ src/App.js
@@ -1,94 +1,14 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Link, Navigate, Routes, Route } from "react-router-dom";
-import { useAuth } from "./component/AuthContext";
-import LoginForm from "./component/LoginForm";
-import IssueManager from "./component/issue/IssueManager";
-import UserDetailManager from './component/user/UserDetailManager';
+import Button from '@mui/material/Button';
+import { AppBar } from '@mui/material';
 
 function App() {
-  const { user } = useAuth()
-
-  const nav = user && [
-    {
-      title: "Home",
-      to: "/",
-    },
-    {
-      title: "Issues",
-      to: "/issues",
-    },
-    {
-      title: "Profile",
-      to: `/users/${user.sub}`,
-    },
-    {
-      title: "Logout",
-      to: "/logout",
-    }
-  ]
-
-  const authenticatedRoutes = (<Router>
+  return (
     <div>
-      <nav>
-        <ul>
-          {nav?.map(navItem => {
-            return <li>
-              <Link to={navItem.to}>{navItem.title}</Link>
-            </li>
-          })}
-        </ul>
-      </nav>
-
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      <Routes>
-        <Route exact path="/issues" element={<IssueManager />}/>
-        <Route exact path={"/users/:username"} element={<UserDetailManager />} />
-        <Route exact path="/Logout" element={<Logout/>} />
-        <Route exact path="/" element={<Home/>} />
-        <Route render={() => <Navigate to="/"/>}/>
-      </Routes>
     </div>
-  </Router>)
-
-  const nonAuthenticatedRoutes = (<Router>
-    <Routes>
-      <Route exact path="/" element={<LoginForm/>} />
-      <Route path="/login" element={<LoginForm/>} />
-      <Route render={() => <Navigate to="/login"/>}/>
-    </Routes>
-  </Router>)
-
-  return (<>{user ? authenticatedRoutes : nonAuthenticatedRoutes}</>)
-
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Logout() {
-  const { removeTokens } = useAuth()
-  removeTokens()
-  return <Navigate to="/"/>;
-}
-
-function Profile() {
-  const { user, token } = useAuth()
-  return <>
-    <h2>Profile</h2>
-    <div>
-      <strong>User</strong> {JSON.stringify(user)}
-    </div>
-    <div>
-      <strong>Token</strong> {token}
-    </div>
-  </>;
+  )
 }
 
 export default App;
--- src/index copy.js
+++ src/index copy.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from "./component/AuthContext";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-  </React.StrictMode>
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
