4e061cb153588ce5b44edeccccee54b40b03d2ca
Unkn0wn126 <32388228+Unkn0wn126@users.noreply.github.com>
2022-04-15 10:35:30 +0200

    Added bases for services

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import React from "react";
 import { BrowserRouter as Router, Link, Navigate, Routes, Route } from "react-router-dom";
 import Button from '@mui/material/Button';
 import { AppBar } from '@mui/material';
+import IssueService from './services/issue.service';
 
 function App() {
+  //IssueService.getAllPublicIssues().then(data => console.log(data));
+  // IssueService.getPublicIssuesByAuthorName("rando").then(data => console.log(data));
+  // IssueService.getPublicIssueById(1).then(data => console.log(data));
+    IssueService.getAllIssues()
+    .then(data => console.log(data))
+    .catch((error) => {
+      console.log(error)
+    });
+
   return (
     <div>
     </div>
---
+++ src/services/issue.service.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const getAllIssues = () => {
+    return axios.get(`${process.env.REACT_APP_BASE_URI}/api/v1/issues/`)
+}
+
+const getIssuesByAuthorName = (authorName) => {
+    return axios.get(`${process.env.REACT_APP_BASE_URI}/api/v1/issues/user/${authorName}`)
+}
+
+const getIssueById = (id) => {
+    return axios.get(`${process.env.REACT_APP_BASE_URI}/api/v1/issues/${id}`)
+}
+
+const createIssue = (issueCreateDto) => {
+    return axios.post(`${process.env.REACT_APP_BASE_URI}/api/v1/issues/create`, issueCreateDto)
+}
+
+const deleteIssue = (id) => {
+    return axios.delete(`${process.env.REACT_APP_BASE_URI}/api/v1/issues/${id}`)
+}
+
+const updateIssue = (id) => {
+    return axios.put(`${process.env.REACT_APP_BASE_URI}/api/v1/issues/${id}`)
+}
+
+const getAllPublicIssues = () => {
+    return axios.get(`${process.env.REACT_APP_BASE_URI}/api/v1/public/issues/`)
+}
+
+const getPublicIssuesByAuthorName = (authorName) => {
+    return axios.get(`${process.env.REACT_APP_BASE_URI}/api/v1/public/issues/user/${authorName}`)
+}
+
+const getPublicIssueById = (id) => {
+    return axios.get(`${process.env.REACT_APP_BASE_URI}/api/v1/public/issues/${id}`)
+}
+
+const IssueService = {
+    getAllIssues,
+    getIssuesByAuthorName,
+    getIssueById,
+    createIssue,
+    deleteIssue,
+    updateIssue,
+    getAllPublicIssues,
+    getPublicIssuesByAuthorName,
+    getPublicIssueById
+}
+
+export default IssueService;
